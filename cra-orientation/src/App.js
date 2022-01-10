fa5a1d761e7ede3ca05870cd22f5440d4e051140
Leah Grim <grimchelate@gmail.com>
2022-01-10 15:09:46 -0600

    notes on react added

---
+++ cra-orientation/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+//we need to import our space jams from the spacejams.js file
+import SpaceJams from './SpaceJams';
+//now in order to see it, we need to use it in the app.js file (see note down below in the return)
+
+function App() {
+  let eagleCamName = 'Dick Pritchett Real Estate';
+
+  //JSX
+  //Javascript eXtended
+  // JSX code is "compiled" into JS code by React
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        {/* render the SpaceJams component */}
+        <SpaceJams />
+       
+        <p> 
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://dickpritchettrealestate.com/"
+          target="_blank" /* Open the link in a new window */
+          rel="noopener noreferrer"
+        >
+          {/* Variable interpolation */} 
+         Monitor some amazing bald eagles and their little stinking eaglets at {eagleCamName}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ cra-orientation/src/SpaceJams.js
@@ -0,0 +1,16 @@
+// this file contains the space Jams component 
+// one component per file
+
+function SpaceJams (){
+    return (
+        <div>
+            <h2> Space Jams is Jamming in Space and Andre is their head Astronaut </h2>
+            {/* any thing you want to render needs to be exported to our DOM and then needs to be imported in the app.js */}
+            <img src= "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/09/12/12/space-jam-internet.jpg?width=990&auto=webp&quality=75&crop=982:726,smart"></img>
+        
+        </div>
+    );
+}
+
+//default export of the file 
+export default SpaceJams;
